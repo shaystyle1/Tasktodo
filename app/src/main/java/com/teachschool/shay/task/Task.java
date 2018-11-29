8b57dd2392e7862d1dcc703368b50a55b88aecd0
DESKTOP-18U4RT6\Shay <shaystyle1@gmail.com>
2018-11-29 19:28:37 +0200

    My First Commit

--- app/src/main/java/com/teachschool/shay/task/Task.java
+++ app/src/main/java/com/teachschool/shay/task/Task.java
@@ -1,4 +1,59 @@
 package com.teachschool.shay.task;
 
 public class Task {
+    String Catagory;
+
+    public String getCatagory() {
+        return Catagory;
+    }
+
+    public void setCatagory(String catagory) {
+        Catagory = catagory;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public void setText(String text) {
+        Text = text;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public int getImportant() {
+        return Important;
+    }
+
+    public void setImportant(int important) {
+        Important = important;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    String Title;
+    String Text;
+    String Date;
+    int Important;
+    String Status;
 }
