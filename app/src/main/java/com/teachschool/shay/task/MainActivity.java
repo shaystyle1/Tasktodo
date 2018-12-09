3633c7319f17a14b691c67f9779cf35fe61741d7
DESKTOP-18U4RT6\Shay <shaystyle1@gmail.com>
2018-12-09 20:28:43 +0200

    My First Commit

--- app/src/main/java/com/teachschool/shay/task/MainActivity.java
+++ app/src/main/java/com/teachschool/shay/task/MainActivity.java
@@ -2,21 +2,75 @@ package com.teachschool.shay.task;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener {
     // Write a message to the database
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("message");
+
+    Spinner catagory;
+    EditText Title;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myRef.setValue("Hello, World!");
+
+        Title =(EditText)findViewById(R.id.Title);
+
+
+        Spinner catagory =(Spinner)findViewById(R.id.catagorys);
+        catagory.setOnItemSelectedListener(this);
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("Categories");
+        categories.add("Work");
+        categories.add("Home");
+        categories.add("School");
+        categories.add("Cars");
+        categories.add("Workout");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        catagory.setAdapter(dataAdapter);
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+
 
     }
 }
